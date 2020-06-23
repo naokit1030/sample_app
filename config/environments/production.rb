1e6c12086a26a645dc95e9d57a5143d4340029b2
naokit1030 <takeshita.na@gmail.com>
2020-06-23 16:07:45 +0000

    Change heroku remote

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fathomless-citadel-90562.herokuapp.com'
+  host = 'pure-springs-77334.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
